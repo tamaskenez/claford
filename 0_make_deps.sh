70c2674a3bd3a1007daea78b2bd5f65738f61a03
Tamas Kenez <tamas.kenez@gmail.com>
2023-02-10 22:39:07 +0100

    v1.0

--- 0_make_deps.sh
+++ 0_make_deps.sh
@@ -2,9 +2,12 @@
 
 rm -rf deps
 mkdir deps
-# git clone https://github.com/emcrisostomo/fswatch --depth 1 deps/fswatch
-git clone https://github.com/tamaskenez/fswatch deps/fswatch/s
+
+# git clone https://github.com/emcrisostomo/fswatch --depth 1 deps/fswatch/s
+git clone https://github.com/tamaskenez/fswatch deps/fswatch/s # using fork until my PR gets merged
+
 git clone https://github.com/boostorg/nowide.git deps/nowide/s --branch standalone --depth 1
+git clone https://github.com/cameron314/readerwriterqueue.git deps/readerwriterqueue/s --depth 1
 
 for build_type in Debug Release; do
     cmake -S deps/fswatch/s -B deps/fswatch/b -DCMAKE_INSTALL_PREFIX=$PWD/i -DCMAKE_DEBUG_POSTFIX=_d -DCMAKE_BUILD_TYPE=$build_type
@@ -12,7 +15,10 @@ for build_type in Debug Release; do
 
     cmake -S deps/nowide/s -B deps/nowide/b -DCMAKE_INSTALL_PREFIX=$PWD/i -DCMAKE_DEBUG_POSTFIX=_d -DCMAKE_BUILD_TYPE=$build_type -DBUILD_TESTING=0
     cmake --build deps/nowide/b --target install --config $build_type -j
-
-    conan install -b missing -g cmake_find_package_multi -if $PWD/i/cmake -s build_type=$build_type .
 done
 
+cmake -S deps/readerwriterqueue/s -B deps/readerwriterqueue/b -DCMAKE_INSTALL_PREFIX=$PWD/i
+cmake --build deps/readerwriterqueue/b --target install
+
+conan install -b missing -g cmake_find_package_multi -if $PWD/i/cmake -s build_type=Debug .
+conan install -b missing -g cmake_find_package_multi -if $PWD/i/cmake -s build_type=Release .
--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,53 @@
 #include <fmt/format.h>
 #include <glog/logging.h>
+#include <readerwriterqueue/readerwriterqueue.h>
 #include <libfswatch/c++/monitor_factory.hpp>
 #include <nowide/args.hpp>
 #include <nowide/cstdlib.hpp>
 #include <nowide/iostream.hpp>
 
+#include <atomic>
+#include <csignal>
 #include <filesystem>
+#include <functional>
+#include <set>
 #include <string_view>
+#include <thread>
+#include <unordered_map>
 
 #define BE(X) (X).begin(), (X).end()
 
-void signal_callback_handler(int signum) {
-    nowide::cerr << "Caught signal " << signum << std::endl;
-    std::exit(signum);
+std::atomic<bool> g_sigint_received;
+
+void signal_handler(int /* signum */) {
+    g_sigint_received = true;
 }
+
+namespace fs = std::filesystem;
+
+constexpr int k_formatting_loop_sleep_millisec = 10;
+constexpr double k_monitor_latency_sec = 0.01;
 
 struct Context {
     struct Options {
         std::vector<std::string> paths;
-        std::vector<std::string> extensions = {
+        std::vector<fs::path> extensions = {
             ".cpp", ".cxx", ".c", ".m", ".mm", ".h", ".hpp", ".hxx"};
+        bool need_extension(const fs::path& e) const {
+            return std::find(BE(extensions), e) != extensions.end();
+        }
     } options;
+    moodycamel::ReaderWriterQueue<std::string> f2m_queue;  // Formatter to monitor queue.
 };
 
 void DisplayHelp() {
     fmt::print("claford - clang-format daemon\n");
-    fmt::print("Watch directories for change and clang-format on hotkey.\n");
+    fmt::print("Watch directories and automatically clang-format changed files.\n");
     fmt::print("Usage: claford [options] paths...\n\n");
     fmt::print("   -h|--help: this help\n");
     fmt::print("\n");
-    fmt::print("paths... is a list of paths (directory or file) to watch\n");
+    fmt::print("paths... is a list of directories to watch\n");
 }
-
-namespace fs = std::filesystem;
 
 std::u8string_view reinterpret_u8(const std::string& s) {
     return std::u8string_view(reinterpret_cast<const char8_t*>(s.c_str()), s.size());
@@ -47,19 +62,6 @@ void fsw_event_callback(const std::vector<fsw::event>& es, void* void_ctx) {
     std::vector<std::string> paths;
     for (auto& e : es) {
         auto path = e.get_path();
-        auto fs_path = fs::path(reinterpret_u8(path));
-        auto ext = std::string(reinterpret_char(fs_path.extension().u8string()));
-        bool found = false;
-        for (auto& oe : ctx->options.extensions) {
-            if (ext == oe) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            continue;
-        }
-        std::string op, ty;
         bool cf = false;
         bool is_file = false;
         for (auto f : e.get_flags()) {
@@ -68,45 +70,24 @@ void fsw_event_callback(const std::vector<fsw::event>& es, void* void_ctx) {
                 case PlatformSpecific:
                     break;
                 case Created:
-                    op += "!";
-                    cf = true;
-                    break;
                 case Updated:
-                    op += "~";
-                    cf = true;
-                    break;
                 case Removed:
-                    op += "x";
-                    cf = true;
-                    break;
                 case Renamed:
-                    op += ">";
                     cf = true;
                     break;
                 case OwnerModified:
                 case AttributeModified:
                     break;
                 case MovedFrom:
-                    op += "F";
-                    cf = true;
-                    break;
                 case MovedTo:
-                    op += "T";
                     cf = true;
                     break;
                 case IsFile:
-                    ty += "F";
                     is_file = true;
                     break;
                 case IsDir:
-                    ty += "D";
-                    break;
                 case IsSymLink:
-                    ty += "S";
-                    break;
                 case Link:
-                    ty += "L";
-                    break;
                 case Overflow:
                     break;
             }
@@ -118,21 +99,53 @@ void fsw_event_callback(const std::vector<fsw::event>& es, void* void_ctx) {
     std::sort(BE(paths));
     paths.erase(std::unique(BE(paths)), paths.end());
     for (auto& p : paths) {
-        if (!fs::exists(reinterpret_u8(p))) {
+        CHECK(ctx->f2m_queue.enqueue(std::move(p)));
+    }
+}
+
+void FormattingLoop(Context& ctx) {
+    std::string path;
+    std::unordered_map<std::string, fs::file_time_type> paths_formatted_at;
+    for (;;) {
+        if (!ctx.f2m_queue.try_dequeue(path)) {
+            if (g_sigint_received) {
+                return;
+            }
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(k_formatting_loop_sleep_millisec));
             continue;
         }
-        int result = nowide::system(fmt::format("clang-format -i {}", p).c_str());
+        // Filter by extension.
+        auto fs_path = fs::path(reinterpret_u8(path));
+        if (!ctx.options.need_extension(fs_path.extension())) {
+            continue;
+        }
+        // Ignore non-existing.
+        if (!fs::exists(reinterpret_u8(path))) {
+            paths_formatted_at.erase(path);
+            continue;
+        }
+        // Ignore files not changed since formatting.
+        auto it = paths_formatted_at.find(path);
+        if (it != paths_formatted_at.end()) {
+            auto last_write_time = fs::last_write_time(path);
+            if (last_write_time == it->second) {
+                continue;
+            }
+        }
+        // Format.
+        int result = system(fmt::format("clang-format -i {}", path).c_str());
         if (result == EXIT_SUCCESS) {
-            nowide::cout << "Formatted " << p << "\n";
+            paths_formatted_at[path] = fs::last_write_time(path);
+            nowide::cout << "Formatted " << path << "\n";
         } else {
-            nowide::cout << "ERROR formatting " << p << "\n";
+            paths_formatted_at.erase(path);
+            nowide::cout << "ERROR formatting " << path << "\n";
         }
     }
 }
 
-int main(int argc, char* argv[]) {
-    signal(SIGINT, signal_callback_handler);
-
+int main_core(int argc, char* argv[]) {
     nowide::args _(argc, argv);
 
     FLAGS_logtostderr = 1;
@@ -155,20 +168,24 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    if (nowide::system("clang-format --version") != EXIT_SUCCESS) {
-        return EXIT_FAILURE;
-    }
-
     if (os.paths.empty()) {
         nowide::cerr << "No path specified.\n";
         return EXIT_FAILURE;
     }
 
+    if (nowide::system("clang-format --version") != EXIT_SUCCESS) {
+        return EXIT_FAILURE;
+    }
+
     int n_invalid_paths = 0;
     for (auto& p : os.paths) {
-        if (!fs::exists(reinterpret_u8(p))) {
+        auto fs_path = fs::path(reinterpret_u8(p));
+        if (!fs::exists(fs_path)) {
             ++n_invalid_paths;
-            nowide::cerr << "ERROR: Path " << p << " doesn't exist\n";
+            nowide::cerr << "ERROR: Path " << p << " doesn't exist.\n";
+        } else if (!fs::is_directory(fs_path)) {
+            ++n_invalid_paths;
+            nowide::cerr << "ERROR: Path " << p << " is not a directory.\n";
         }
     }
     if (n_invalid_paths > 0) {
@@ -183,9 +200,33 @@ int main(int argc, char* argv[]) {
         return EXIT_FAILURE;
     }
 
-    monitor->set_latency(0.1);
+    monitor->set_latency(k_monitor_latency_sec);
     monitor->set_recursive(true);
-    monitor->start();
+
+    std::signal(SIGINT, signal_handler);
+
+    auto monitor_thread = std::thread([monitor]() {
+        monitor->start();  // Enters event loop, returns when stopped.
+    });
+
+    fmt::print("claford is running, CTRL-C to exit...\n");
+    FormattingLoop(ctx);
+    monitor->stop();
+
+    if (monitor_thread.joinable()) {
+        monitor_thread.join();
+    }
 
     return EXIT_SUCCESS;
+}
+
+int main(int argc, char* argv[]) {
+    try {
+        return main_core(argc, argv);
+    } catch (std::exception& e) {
+        nowide::cerr << "Terminating with exception: " << e.what() << "\n";
+    } catch (...) {
+        nowide::cerr << "Terminating with unknown exception.\n";
+    }
+    return EXIT_FAILURE;
 }
